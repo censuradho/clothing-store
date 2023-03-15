e290ed610598534e1ec65b0adfa9e2a040f8da48
Censuradho <gustavoleiteoliveira800@gmail.com>
2023-03-15 14:58:11 -0300

    chore(pdp)

---
+++ src/layout/category/index.tsx
@@ -0,0 +1,8 @@
+import * as Styles from './styles'
+import { CategoryProps } from './types'
+
+export function CategoryLayout (props: CategoryProps) {
+  return (
+    <Styles.Container></Styles.Container>
+  )
+}
---
+++ src/pages/c/[slug].tsx
@@ -0,0 +1,7 @@
+import { CategoryLayout } from "@/layout/category";
+
+export default function CategoryPage () {
+  return (
+    <CategoryLayout data={null} />
+  )
+}
